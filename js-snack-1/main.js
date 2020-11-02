82f2517314e214b32f74e9439a33952603be98ef
Annalisa <annalisadesantis770@gmail.com>
2020-11-02 16:37:07 +0100

    snack1

--- js-snack-1/main.js
+++ js-snack-1/main.js
@@ -22,6 +22,34 @@ $(document).ready(function() {
        },
     ];
 
+
+    // Creo una variabile minore ed inserisco il valore peso del primo oggetto
+    var min = bici_corsa[0].peso;
+    // Creo una varibile nome per salvare di volta in volta il nome della bici che salviamo nella varibile min
+    var nome_bici;
+
+    // scorro l'array e verifico i peso di ogni bici
+    // se il peso è minore di quello di quello dentro la variabile min allora aggiorno il min, altrimenti vado a quello successivo
+    for (var i = 0; i < bici_corsa.length; i++) {
+
+        var biciCorrente = bici_corsa[i];
+
+        var peso = biciCorrente.peso;
+
+        var nome_corrente = biciCorrente.nome;
+
+        // se peso è minore di min aggiorno il valore di min
+        if (peso < min) {
+
+            // vuol dire che questo è il peso minore trovao fino ad ora
+            min = peso;
+            nome_bici = nome_corrente;
+        }
+    }
+
+    $("h1").text("La bici più leggera è: " + nome_bici);
     
+    console.log(min);
+    console.log(nome_bici);
 
 });
