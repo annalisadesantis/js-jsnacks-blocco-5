d76e0f498e04c5b0032d38f1dd961b9ed4854beb
Annalisa <annalisadesantis770@gmail.com>
2020-11-02 16:07:19 +0100

    creazione file + array object

---
+++ js-snack-1/main.js
@@ -0,0 +1,27 @@
+// Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
+
+$(document).ready(function() {
+
+    var bici_corsa =
+    [
+       {
+          nome : "bianchi-aria-disc",
+          peso : 8,
+       },
+       {
+           nome : "Bh-Quartz",
+           peso : 7,
+       },
+       {
+           nome : "B’Twin-Ultra",
+           peso : 6,
+       },
+       {
+           nome : "Cannondale-Super-Six-Evo",
+           peso : 9,
+       },
+    ];
+
+    
+
+});
